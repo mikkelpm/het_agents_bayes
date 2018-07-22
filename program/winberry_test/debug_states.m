3eeaab9fd3d7b3bcaac3c57c2ff18caa5c6096e4
Laura <liuyu1237@gmail.com>
2018-07-22 01:08:28 -0400

    net income: debug - add true asset to dist

--- program/winberry_test/debug_states.m
+++ program/winberry_test/debug_states.m
@@ -16,57 +16,57 @@ n_aplot = 100;
 v_aplot = [linspace(0,max(a_aux(e_aux==0)),n_aplot);
     linspace(0,max(a_aux(e_aux==1)),n_aplot)];
 
-% %% smooth draws
-% all_states = nan(T_hh, n_smooth_vars, num_smooth_draws);
-% all_fplot = nan(n_aplot,num_smooth_draws,2,T_hh);
-% 
-% for i_draw = 1:num_smooth_draws
-%     % parfor i_draw = 1:num_smooth_draws
-%     
-%     dataset_fake = struct;
-%     dataset_fake.nobs = nobs;
-%     
-%     % Compute smoothing draw
-%     the_smooth_draw = simulation_smoother(smooth_means, smooth_vars, num_burnin_periods, rand_seeds(i_draw), ...
-%         M_new, oo_new, options_new, dataset_fake, dataset_info, xparam1, estim_params_, bayestopt_);
-%     for iter_j=1:n_smooth_vars
-%         the_var = deblank(smooth_vars(iter_j,:));
-%         all_states(:,iter_j,i_draw) = the_smooth_draw.(the_var)(ts_hh);
-%     end
-%     
-%     
-%     for it = 1:T_hh
-%         
-%         t = ts_hh(it);
-%         
-%         for eepsilon = 0:1
-%             
-%             % probability
-%             % prepare the parameters
-%             moment = nan(1,nMeasure_local);
-%             measureCoefficient = nan(1,nMeasure_local);
-%             for i_Measure = 1:nMeasure_local
-%                 moment(i_Measure) = the_smooth_draw.(['moment_' num2str(eepsilon+1) '_' num2str(i_Measure)])(t);
-%                 measureCoefficient(i_Measure) = the_smooth_draw.(['measureCoefficient_' num2str(eepsilon+1) '_' num2str(i_Measure)])(t);
-%             end
-%             
-%             % Compute normalization constant
-%             moment_aux = moment;
-%             moment_aux(1) = 0;
-%             g = @(a) exp(measureCoefficient*((a-moment(1)).^((1:nMeasure_local)')-moment_aux'));
-%             lastwarn('');
-%             normalization = integral(g, aaBar_local, Inf);
-%             warnMsg = lastwarn;
-%             if ~isempty(warnMsg)
-%                 disp(measureCoefficient);
-%                 error('Improper asset density');
-%             end
-%             
-%             all_fplot(:,i_draw,eepsilon+1,it) = g(v_aplot(eepsilon+1,:))/normalization;
-%             
-%         end
-%     end
-% end
+%% smooth draws
+all_states = nan(T_hh, n_smooth_vars, num_smooth_draws);
+all_fplot = nan(n_aplot,num_smooth_draws,2,T_hh);
+
+for i_draw = 1:num_smooth_draws
+    % parfor i_draw = 1:num_smooth_draws
+    
+    dataset_fake = struct;
+    dataset_fake.nobs = nobs;
+    
+    % Compute smoothing draw
+    the_smooth_draw = simulation_smoother(smooth_means, smooth_vars, num_burnin_periods, rand_seeds(i_draw), ...
+        M_new, oo_new, options_new, dataset_fake, dataset_info, xparam1, estim_params_, bayestopt_);
+    for iter_j=1:n_smooth_vars
+        the_var = deblank(smooth_vars(iter_j,:));
+        all_states(:,iter_j,i_draw) = the_smooth_draw.(the_var)(ts_hh);
+    end
+    
+    
+    for it = 1:T_hh
+        
+        t = ts_hh(it);
+        
+        for eepsilon = 0:1
+            
+            % probability
+            % prepare the parameters
+            moment = nan(1,nMeasure_local);
+            measureCoefficient = nan(1,nMeasure_local);
+            for i_Measure = 1:nMeasure_local
+                moment(i_Measure) = the_smooth_draw.(['moment_' num2str(eepsilon+1) '_' num2str(i_Measure)])(t);
+                measureCoefficient(i_Measure) = the_smooth_draw.(['measureCoefficient_' num2str(eepsilon+1) '_' num2str(i_Measure)])(t);
+            end
+            
+            % Compute normalization constant
+            moment_aux = moment;
+            moment_aux(1) = 0;
+            g = @(a) exp(measureCoefficient*((a-moment(1)).^((1:nMeasure_local)')-moment_aux'));
+            lastwarn('');
+            normalization = integral(g, aaBar_local, Inf);
+            warnMsg = lastwarn;
+            if ~isempty(warnMsg)
+                disp(measureCoefficient);
+                error('Improper asset density');
+            end
+            
+            all_fplot(:,i_draw,eepsilon+1,it) = g(v_aplot(eepsilon+1,:))/normalization;
+            
+        end
+    end
+end
 
 %% true states
 benchmark = load([debug_folder 'simul.mat']);
@@ -170,25 +170,27 @@ benchmark_ll = sum(the_loglikes_hh_draw)
 %     print(h1,[debug_folder 'state_' the_var],'-dpng');
 % end
 % 
-% %% plot state
-% 
-% for it = 1:T_hh
-%     for eepsilon = 0:1
-%         h1 = figure;
-%         set(h1,'Units','Inches','position',[0 0 6 6])
-%         set(h1,'PaperOrientation','portrait');
-%         set(h1,'PaperPositionMode','Auto');
-%         set(h1,'Clipping','off')
-%         SFont = 16;
-%         hold on
-%         for i_draw = 1:num_smooth_draws
-%             patchline(v_aplot(eepsilon+1,:),all_fplot(:,i_draw,eepsilon+1,it),'linestyle','-','edgecolor',[.7 .7 .7],'linewidth',1,'edgealpha',0.05);
-%         end
-%         plot(v_aplot(eepsilon+1,:),benchmark_fplot(:,eepsilon+1,it),'k-','linewidth',2);
-%         hold off
-%         xlim(v_aplot(eepsilon+1,[1 end]))
-%         set(gca,'FontSize',SFont,'FontWeight','bold')
-%         print(h1,[debug_folder 't' num2str(ts_hh(it)) 'e' num2str(eepsilon)],'-dpng');
-% 
-%     end
-% end
+%% plot dist
+
+for it = 1:T_hh
+    for eepsilon = 0:1
+        h1 = figure;
+        set(h1,'Units','Inches','position',[0 0 6 6])
+        set(h1,'PaperOrientation','portrait');
+        set(h1,'PaperPositionMode','Auto');
+        set(h1,'Clipping','off')
+        SFont = 16;
+        hold on
+        for i_draw = 1:num_smooth_draws
+            patchline(v_aplot(eepsilon+1,:),all_fplot(:,i_draw,eepsilon+1,it),'linestyle','-','edgecolor',[.7 .7 .7],'linewidth',1,'edgealpha',0.05);
+        end
+        plot(v_aplot(eepsilon+1,:),benchmark_fplot(:,eepsilon+1,it),'k-','linewidth',2);
+        [ff,xx] = ksdensity(data_hh(it,data_hh(it,:,1)==eepsilon,3));
+        plot(xx,ff,'b-','linewidth',2)
+        hold off
+        xlim(v_aplot(eepsilon+1,[1 end]))
+        set(gca,'FontSize',SFont,'FontWeight','bold')
+        print(h1,[debug_folder 't' num2str(ts_hh(it)) 'e' num2str(eepsilon)],'-dpng');
+
+    end
+end
