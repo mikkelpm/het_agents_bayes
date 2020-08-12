ac4000b9d45628aca67957a13c85faea807070b1
mikkelpm <mikkel.p.m@gmail.com>
2020-08-12 16:19:10 -0400

    Also adjust firm model micro likelihood

--- program/firm_model/auxiliary_functions/likelihood/likelihood_micro.m
+++ program/firm_model/auxiliary_functions/likelihood/likelihood_micro.m
@@ -1,16 +1,22 @@
-function likes = likelihood_micro(smooth_draw, t, data_micro, it, nnu, ttheta, trunc_logn)
+function likes = likelihood_micro(smooth_draw_t, data_micro_t, param)
 
     % Micro likelihood for firm model
-    
-    c = log(nnu)+smooth_draw.aggregateTFP(t)-smooth_draw.logWage(t);
-    jacob = 1-nnu;
-    ix = ~isnan(data_micro(it,:,1));
-    produ = jacob*data_micro(it,ix,1)-c-ttheta*data_micro(it,ix,2);
 
-    the_mean = [smooth_draw.lag_moment_1(t) smooth_draw.lag_moment_2(t)];
-    the_varcov = [smooth_draw.lag_moment_3(t) smooth_draw.lag_moment_4(t);
-                  smooth_draw.lag_moment_4(t) smooth_draw.lag_moment_5(t)];
-    likes = jacob*mvnpdf([produ' data_micro(it,ix,2)'], the_mean, the_varcov);
+    % Read parameters
+    nnu = param(1);
+    ttheta = param(2);
+    trunc_logn = param(3);
+
+    % Likelihood contribution
+    c = log(nnu)+smooth_draw_t{1,'aggregateTFP'}-smooth_draw_t{1,'logWage'};
+    jacob = 1-nnu;
+    ix = ~isnan(data_micro_t(:,1));
+    produ = jacob*data_micro_t(ix,1)-c-ttheta*data_micro_t(ix,2);
+
+    the_mean = smooth_draw_t{1,{'lag_moment_1','lag_moment_2'}};
+    the_varcov = [smooth_draw_t{1,{'lag_moment_3','lag_moment_4'}};
+                  smooth_draw_t{1,{'lag_moment_4','lag_moment_5'}}];
+    likes = jacob*mvnpdf([produ data_micro_t(ix,2)], the_mean, the_varcov);
 
     % Truncation term
     the_aux = [1 ttheta]';
