251a3de198dac6637835d910152d534834afe91a
mikkelpm <mikkel.p.m@gmail.com>
2020-09-21 18:14:43 -0400

    Plot settings: treat parameters and polfct/distr symmetrically

--- program/plot_mcmc.m
+++ program/plot_mcmc.m
@@ -19,6 +19,7 @@ n_liktype = length(plot_liktypes);
 n_rep = length(plot_reps);
 
 % Reporting settings
+is_plot_param = true;           % Plot MCMC results for model parameters?
 plot_burnin = 1e3;              % Burn-in for MCMC chains
 acf_lags = 200;                 % No. of ACF lags
 
@@ -45,9 +46,9 @@ else
 end
 
 % Extra posterior computations (only for 'hh' model)
-is_polfct_distirf = true;       % Compute consumption policy function and distribution IRF
+is_plot_polfct_distirf = true;  % Plot consumption policy function and distribution IRF
+is_run_comput = true;           % true: run computations; false: load previous computations from file
 is_run_dynare = false;          % Process Dynare model?
-is_run_comput = true;           % true: run computation; false: load previous computations from file
 comput_rep = 1;                 % Single repetition to use for computations (must be included in "plot_reps")
 dynare_model = 'firstOrderDynamics_polynomials'; % Dynare model file for 'hh' model
 thin_draw = 10;                 % Compute consumption policy or distribution every X-th draw
@@ -113,127 +114,131 @@ clearvars the_file;
 %% ACF, trace plot, and posterior density
 
 mkdir(save_folder);
-nparam_all = length(tex_param);
 
-f_postdens_all = figure;
-n_rep_actual = 0;
-stack_postdens_all = cell(nparam_all,1); % For layers of hairlines    
+if is_plot_param
 
-for i_rep = 1:n_rep
-    
-    f_postdens = figure;
-    the_n_liktype = 0;
+    f_postdens_all = figure;
+    n_rep_actual = 0;
+    nparam_all = length(tex_param);
+    stack_postdens_all = cell(nparam_all,1); % For layers of hairlines    
 
-    for i_type = 1:n_liktype
-        
-        if isempty(model_mcmc{i_rep,i_type})
-            continue;
-        end
-        
-        the_n_liktype = the_n_liktype+1;
-        the_nparam = model_nparam(i_rep,i_type); % Number of parameters for this likelihood type
-        i_layer = find(layer_order==i_type);
-        
-        f_trace = figure;
-        f_acf = figure;
-        
-        for i_param = 1:the_nparam
-            
-            the_draws = model_mcmc{i_rep,i_type}.post_draws(:,i_param); % Posterior draws for parameter
-            the_param = find(strcmp(plot_param,model_param_names{i_rep,i_type}(i_param)),1); % Index of parameter in full parameter vector
-            the_tex = tex_param{the_param}; % Tex title for parameter
-            
-            % Trace plot
-            figure(f_trace);
-            subplot(the_nparam,1,i_param);
-            plot(the_draws,'-k');
-            xline(plot_burnin,'k--');
-            title(the_tex,'FontSize',plot_fontsize,'FontWeight','bold');
-            
-            % ACF
-            figure(f_acf);
-            subplot(the_nparam,1,i_param);
-            the_acf = autocorr(the_draws(plot_burnin+1:end),acf_lags);
-            plot(0:acf_lags,the_acf,'-k','LineWidth',2);
-            yline(0, 'k--');
-            title(the_tex,'FontSize',plot_fontsize,'FontWeight','bold');
-            
-            % Posterior density
-            figure(f_postdens);
-            subplot(1,nparam_all,the_param);
-            [the_f,the_xi] = ksdensity(the_draws(plot_burnin+1:end));
-            plot(the_xi,the_f,'LineStyle',linestyles_postdens_1rep{i_type},...
-                'LineWidth',2,'Color',colors_postdens(i_type,:));
-            if isempty(get(get(gca,'Title'),'String'))
-                hold on; % hold off will be automatic when the figure is closed
-                xline(model_params_truth{i_rep,i_type}(i_param),'k--');
-                title(the_tex,'FontSize',plot_fontsize,'FontWeight','bold');
+    for i_rep = 1:n_rep
+
+        f_postdens = figure;
+        the_n_liktype = 0;
+
+        for i_type = 1:n_liktype
+
+            if isempty(model_mcmc{i_rep,i_type})
+                continue;
             end
-            
-            % Posterior density, all repetitions together
-            figure(f_postdens_all);
-            subplot(1,nparam_all,the_param);
-            patchline(the_xi,the_f,'linestyle','-','edgecolor',colors_postdens(i_type,:),...
-                      'linewidth',1,'edgealpha',alpha_postdens);
-            stack_postdens_all{the_param} = [i_layer stack_postdens_all{the_param}];
-            if isempty(get(get(gca,'Title'),'String'))
-                hold on; % hold off will be automatic when the figure is closed
-                xline(model_params_truth{i_rep,i_type}(i_param),'k--');
-                stack_postdens_all{the_param} = [n_liktype+1 stack_postdens_all{the_param}]; % Bottom of the layer
+
+            the_n_liktype = the_n_liktype+1;
+            the_nparam = model_nparam(i_rep,i_type); % Number of parameters for this likelihood type
+            i_layer = find(layer_order==i_type);
+
+            f_trace = figure;
+            f_acf = figure;
+
+            for i_param = 1:the_nparam
+
+                the_draws = model_mcmc{i_rep,i_type}.post_draws(:,i_param); % Posterior draws for parameter
+                the_param = find(strcmp(plot_param,model_param_names{i_rep,i_type}(i_param)),1); % Index of parameter in full parameter vector
+                the_tex = tex_param{the_param}; % Tex title for parameter
+
+                % Trace plot
+                figure(f_trace);
+                subplot(the_nparam,1,i_param);
+                plot(the_draws,'-k');
+                xline(plot_burnin,'k--');
                 title(the_tex,'FontSize',plot_fontsize,'FontWeight','bold');
+
+                % ACF
+                figure(f_acf);
+                subplot(the_nparam,1,i_param);
+                the_acf = autocorr(the_draws(plot_burnin+1:end),acf_lags);
+                plot(0:acf_lags,the_acf,'-k','LineWidth',2);
+                yline(0, 'k--');
+                title(the_tex,'FontSize',plot_fontsize,'FontWeight','bold');
+
+                % Posterior density
+                figure(f_postdens);
+                subplot(1,nparam_all,the_param);
+                [the_f,the_xi] = ksdensity(the_draws(plot_burnin+1:end));
+                plot(the_xi,the_f,'LineStyle',linestyles_postdens_1rep{i_type},...
+                    'LineWidth',2,'Color',colors_postdens(i_type,:));
+                if isempty(get(get(gca,'Title'),'String'))
+                    hold on; % hold off will be automatic when the figure is closed
+                    xline(model_params_truth{i_rep,i_type}(i_param),'k--');
+                    title(the_tex,'FontSize',plot_fontsize,'FontWeight','bold');
+                end
+
+                % Posterior density, all repetitions together
+                figure(f_postdens_all);
+                subplot(1,nparam_all,the_param);
+                patchline(the_xi,the_f,'linestyle','-','edgecolor',colors_postdens(i_type,:),...
+                          'linewidth',1,'edgealpha',alpha_postdens);
+                stack_postdens_all{the_param} = [i_layer stack_postdens_all{the_param}];
+                if isempty(get(get(gca,'Title'),'String'))
+                    hold on; % hold off will be automatic when the figure is closed
+                    xline(model_params_truth{i_rep,i_type}(i_param),'k--');
+                    stack_postdens_all{the_param} = [n_liktype+1 stack_postdens_all{the_param}]; % Bottom of the layer
+                    title(the_tex,'FontSize',plot_fontsize,'FontWeight','bold');
+                end
+
             end
-            
+
+            % Save trace plot and ACF
+            graph_out(f_trace,fullfile(save_folder,strcat(model_filename{i_rep,i_type},'_trace')),graph_size_diagnostic);
+            graph_out(f_acf,fullfile(save_folder,strcat(model_filename{i_rep,i_type},'_acf')),graph_size_diagnostic);
+
         end
-        
-        % Save trace plot and ACF
-        graph_out(f_trace,fullfile(save_folder,strcat(model_filename{i_rep,i_type},'_trace')),graph_size_diagnostic);
-        graph_out(f_acf,fullfile(save_folder,strcat(model_filename{i_rep,i_type},'_acf')),graph_size_diagnostic);
-        
+
+        % Save posterior densities
+        if the_n_liktype>0
+
+            graph_out(f_postdens, ...
+                      fullfile(save_folder,...
+                               strcat(strrep(model_filename{i_rep,1},sprintf('%s%d','_liktype',plot_liktypes(1)),''), ...
+                                      '_postdens') ...
+                               ), ...
+                      graph_size_postdens);
+
+            n_rep_actual = n_rep_actual+1;
+
+        else
+            close(f_postdens);
+        end
+
     end
-    
-    % Save posterior densities
-    if the_n_liktype>0
-        
-        graph_out(f_postdens, ...
-                  fullfile(save_folder,...
-                           strcat(strrep(model_filename{i_rep,1},sprintf('%s%d','_liktype',plot_liktypes(1)),''), ...
-                                  '_postdens') ...
-                           ), ...
-                  graph_size_postdens);
-              
-        n_rep_actual = n_rep_actual+1;
-        
+
+    % Save posterior densities, all repetitions together
+    if n_rep_actual>1 
+
+        % Reorder the layers
+        figure(f_postdens_all);
+        for the_param = 1:nparam_all
+            [~,children_order] = sort(stack_postdens_all{the_param});
+            subplot(1,nparam_all,the_param);
+            children_handle = get(gca,'Children');
+            set(gca,'Children',children_handle(children_order))
+        end
+
+        graph_out(f_postdens_all,fullfile(save_folder,strcat(model_filename{i_rep,1}(1:end-11),'postdens')),...
+                  graph_size_postdens);   
+
     else
-        close(f_postdens);
+        close(f_postdens_all);        
     end
-    
+
+    clearvars the_*;
+
 end
-
-% Save posterior densities, all repetitions together
-if n_rep_actual>1 
-    
-    % Reorder the layers
-    figure(f_postdens_all);
-    for the_param = 1:nparam_all
-        [~,children_order] = sort(stack_postdens_all{the_param});
-        subplot(1,nparam_all,the_param);
-        children_handle = get(gca,'Children');
-        set(gca,'Children',children_handle(children_order))
-    end
-    
-    graph_out(f_postdens_all,fullfile(save_folder,strcat(model_filename{i_rep,1}(1:end-11),'postdens')),...
-              graph_size_postdens);   
-
-else
-    close(f_postdens_all);        
-end
-
-clearvars the_*;
 
 
 %% Consumption policy function and distribution IRF
 
-if strcmp(model_name, 'hh') && is_polfct_distirf
+if strcmp(model_name, 'hh') && is_plot_polfct_distirf
     
     addpath('functions');
     addpath(fullfile('functions', 'likelihood'));
